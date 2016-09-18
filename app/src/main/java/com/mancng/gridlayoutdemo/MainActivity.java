134587265a29d81fd51f15b0a8850c7392fa5eb2
runrmrf <runumrf@gmail.com>
2016-09-18 00:16:54 -0700

    GridLayoutDemo

---
+++ app/src/main/java/com/mancng/gridlayoutdemo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.mancng.gridlayoutdemo;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnOnClick(View view) {
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.mancng.gridlayoutdemo");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
+        Log.i("button tapped", ourId);
+    }
+}
+
